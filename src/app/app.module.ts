60608f2483e262d7d58703d3891615e3c90fd538
Luis Reyes <luisandresreyes@hotmail.com>
2024-03-20 13:32:34 -0300

    Fin desafio 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    AlumnosComponent,
   ],
   imports: [
     BrowserModule,
